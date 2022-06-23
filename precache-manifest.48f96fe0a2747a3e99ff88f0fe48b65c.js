001c8b5282688810f0b104c1482404b40ac51ac2
viraj-mahida <virajmmahida382002@gmail.com>
2022-06-23 14:56:07 +0530

    Updates

---
+++ precache-manifest.48f96fe0a2747a3e99ff88f0fe48b65c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a7c04b1291c8b8380918",
+    "url": "/Travel-Square-Holidays/static/css/main.d0f1e066.chunk.css"
+  },
+  {
+    "revision": "a7c04b1291c8b8380918",
+    "url": "/Travel-Square-Holidays/static/js/main.a7c04b12.chunk.js"
+  },
+  {
+    "revision": "206e060bd7d3afd2158d",
+    "url": "/Travel-Square-Holidays/static/js/1.206e060b.chunk.js"
+  },
+  {
+    "revision": "884c2dce7f821c8d4736",
+    "url": "/Travel-Square-Holidays/static/js/2.884c2dce.chunk.js"
+  },
+  {
+    "revision": "69e0d8c3c1d7b1398e16",
+    "url": "/Travel-Square-Holidays/static/js/runtime~main.69e0d8c3.js"
+  },
+  {
+    "revision": "70c0eed54d9ee5346c571eec936e56ec",
+    "url": "/Travel-Square-Holidays/static/media/thailabdPackage.70c0eed5.jpg"
+  },
+  {
+    "revision": "e46b26f5db4ec4c5cd444890438ca620",
+    "url": "/Travel-Square-Holidays/static/media/pattayaPackage.e46b26f5.jpg"
+  },
+  {
+    "revision": "825e192e93ad7a5cffee2755d99376c8",
+    "url": "/Travel-Square-Holidays/static/media/dubaiPackage.825e192e.jpg"
+  },
+  {
+    "revision": "71fccc525fefb89071c113f06b0fab75",
+    "url": "/Travel-Square-Holidays/static/media/Whyus1.71fccc52.jpg"
+  },
+  {
+    "revision": "8030ebc2d854b8f1fbde8d905da8bc2b",
+    "url": "/Travel-Square-Holidays/static/media/Whyus2.8030ebc2.jpg"
+  },
+  {
+    "revision": "a7d6d279c1e8611828b6385ed7a46c09",
+    "url": "/Travel-Square-Holidays/static/media/Whyus3.a7d6d279.jpg"
+  },
+  {
+    "revision": "87b8d4c04e0049296d26c05a84a5b292",
+    "url": "/Travel-Square-Holidays/static/media/Aboutus.87b8d4c0.jpg"
+  },
+  {
+    "revision": "e1308aacf373908bb2bb079897106a2b",
+    "url": "/Travel-Square-Holidays/static/media/Dubai.e1308aac.jpg"
+  },
+  {
+    "revision": "21d84d29aae3c06d3fb1ce1f9f7dcdf0",
+    "url": "/Travel-Square-Holidays/index.html"
+  }
+];
